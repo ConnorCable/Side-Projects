1104aa0c209edd665652599214c1b71a7334340d
ctcmc <ccab0528@gmail.com>
2020-12-11 14:44:28 -0800

    Add HTML, CSS, JS files

---
+++ Etch-A-Sketch/etchasketch.js
@@ -0,0 +1,13 @@
+let number = 4
+      var container = document.getElementById("contain")
+      var box = document.createElement("div")
+        box.setAttribute("class" , "col")
+
+      
+      
+      for(var i = 0; i < 7 ; i++){
+        var row = document.createElement("div")
+        row.setAttribute("class" , "row")
+        row.setAttribute("id", "blocks")
+      }
+ 
